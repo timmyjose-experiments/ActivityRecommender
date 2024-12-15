4e792d5208a283bae72be731e8673ff1622b8343
Timmy Jose <zoltan.jose@gmail.com>
2024-12-15 19:38:52 +0530

    chore: basic app clip demo; app overlay launch demo (pointing to Payy instead)

--- ActivityRecommenderAppClip/ContentView.swift
+++ ActivityRecommenderAppClip/ContentView.swift
@@ -6,53 +6,68 @@
 //
 
 import SwiftUI
+import StoreKit
 
 struct ContentView: View {
     var activities = ["Archery", "Baseball"]
     @State private var selectedActivity = "Archery"
+    
+    @State private var activityCount = 0
+    @State private var showUpgradePrompt = false
     
     var colors: [Color] = [.blue, .orange]
     
     @State private var id = 1
     
     var body: some View {
-        Spacer()
-        
-        Text("(Running inside an app clip)")
-            .font(.title3)
-            .onContinueUserActivity(NSUserActivityTypeBrowsingWeb, perform: handleUserActivity(_:))
-        
-        Text("Why not try...")
-            .font(.largeTitle.bold())
-        
-        
         VStack {
-            Circle()
-                .fill(colors.randomElement() ?? .blue)
-                .padding()
-                .overlay(
-                    Image(systemName: "figure.\(selectedActivity.lowercased())")
-                        .font(.system(size: 144))
-                        .foregroundColor(.white)
-                )
+            Spacer()
             
-            Text("\(selectedActivity)!")
-                .font(.title)
-        }
-        .transition(.slide)
-        .id(id)
-        
-        Spacer()
-        
-        Button("Try again") {
-            withAnimation(.easeInOut(duration: 1)) {
-                selectedActivity = activities.randomElement() ?? "Archery"
-                id += 1
+            Text("(Running inside an app clip)")
+                .font(.title3)
+                .onContinueUserActivity(NSUserActivityTypeBrowsingWeb, perform: handleUserActivity(_:))
+            
+            Text("Why not try...")
+                .font(.largeTitle.bold())
+            
+            
+            VStack {
+                Circle()
+                    .fill(colors.randomElement() ?? .blue)
+                    .padding()
+                    .overlay(
+                        Image(systemName: "figure.\(selectedActivity.lowercased())")
+                            .font(.system(size: 144))
+                            .foregroundColor(.white)
+                    )
+                
+                Text("\(selectedActivity)!")
+                    .font(.title)
             }
-        }
+            .transition(.slide)
+            .id(id)
+            
+            Spacer()
+            
+            Button("Try again") {
+                withAnimation(.easeInOut(duration: 1)) {
+                    selectedActivity = activities.randomElement() ?? "Archery"
+                    id += 1
+                    
+                    activityCount += 1
+                    
+                    if activityCount >= 5 {
+                        showUpgradePrompt = true
+                    }
+                }
+            }
             .buttonStyle(.borderedProminent)
-        
-        Spacer()
+            
+            Spacer()
+            
+        }.appStoreOverlay(isPresented: $showUpgradePrompt) {
+            SKOverlay.AppConfiguration(appIdentifier: "6474760518", position: .bottom)
+        }
     }
     
     private func handleUserActivity(_ activity: NSUserActivity?) {
